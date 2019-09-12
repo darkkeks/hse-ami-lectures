2ebccd0b22f8c0137eb5af9f8f7843396dfceea6
darkkeks <darkkeks@rambler.ru>
2019-09-12 22:48:03 +0300

    Altered build layout

--- build_all.sh
+++ build_all.sh
@@ -2,13 +2,17 @@
 
 build() {
     pushd $1
-    latexmk -pdf -interaction=nonstopmode -halt-on-error $1.tex 
-    cp *.pdf ../_build/
+    latexmk -pdf -interaction=nonstopmode -halt-on-error $2.tex 
     latexmk -c
     popd
+    mkdir -p _build/$1
+    cp $1/$2.pdf _build/$1
 }
 
 mkdir _build
 
-build linear_algebra
-build mathematical_analysis
+build linear_algebra linear_algebra
+
+build mathematical_analysis mathematical_analysis
+build mathematical_analysis/homework homework_1
+
